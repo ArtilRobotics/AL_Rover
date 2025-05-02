bf58a1d4566c9004c04849f12ad8db96995a5052
abned <andyg.sak@gmail.com>
2025-05-02 09:29:05 -0500

    remove: build folders, and add: gitignore

--- Motor_Go2_control/build/CMakeFiles/identifica_u2d2.dir/compiler_depend.ts
+++
@@ -1,2 +0,0 @@
-# CMAKE generated file: DO NOT EDIT!
-# Timestamp file for compiler generated dependencies management for identifica_u2d2.
--- Motor_Go2_control/build/CMakeFiles/listar_puertos.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for listar_puertos.
-# This may be replaced when dependencies are built.
